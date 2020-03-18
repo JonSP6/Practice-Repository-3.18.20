5aa63deab1a03a51715ef30082e466293e85e1d5
JonSP6 <51219855+JonSP6@users.noreply.github.com>
2020-03-18 18:53:21 -0400

    Practice commit of new files

---
+++ script_practice2.js
@@ -0,0 +1,57 @@
+// Anki Flashcard
+var human = {
+    name: "jon",
+    age: 35,
+    hobby: 'soccer',
+    isMarried: false,
+    shout: function () {
+        console.log('AHHHHH!');
+    },
+    spells: ["abracadabra", "shazam", "boo"],
+};
+
+
+var todos = [
+    'clean room',
+    'brush teeth',
+    'exercise',
+    'study javascript',
+    'eat healthy',
+];
+
+
+var languages = {
+    first: "C",
+    second: "Java",
+    third: "Python",
+    fourth: "PHP",
+    fifth: "JavaScript",
+};
+
+// var list = [
+//     {
+//         username: "andy",
+//         password: "secret",
+//     },
+//     {
+//         username: "jess",
+//         password: "123",
+//     },
+// ];
+
+
+// ****************************************************************************
+// Practice
+// ctl + shift + l
+
+//Assignment Operator
+// a += b
+// Same as
+// // a = a + b
+
+//initial pour bloom
+//udemy photography
+
+
+
+
